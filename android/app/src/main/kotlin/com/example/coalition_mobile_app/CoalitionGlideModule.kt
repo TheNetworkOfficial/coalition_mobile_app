b48f1a8352e72ef8ed4e68ab1950188e233a2ffc
Lux <99502626+TheNetworkOfficial@users.noreply.github.com>
2025-10-01 09:43:19 -0600

    Add Glide module and annotation processor

---
+++ android/app/src/main/kotlin/com/example/coalition_mobile_app/CoalitionGlideModule.kt
@@ -0,0 +1,7 @@
+package com.example.coalition_mobile_app
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class CoalitionGlideModule : AppGlideModule()
